36163a181b9c80dff1acbb35c4efecc7de9224c3
kyh012412 <kyh0124.12@gmail.com>
2024-03-09 17:04:35 +0900

    3.6end

--- app.js
+++ app.js
@@ -6,51 +6,59 @@ console.log(h1);
 h1.innerText = 'Hello';
 
 function handleHelloClick() {
-  h1.style.color = 'blue';
-  console.log('title was clicked');
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor != 'blue') {
+    newColor = 'blue';
+  } else {
+    newColor = 'tomato';
+  }
+  h1.style.color = newColor;
 }
 
 // hello.onclick = handleHelloClick;
 h1.addEventListener('click', handleHelloClick);
 
-//onmouseenter
-// hover처럼? 클릭이아니라 마우스가 요소의 범위내로 들어왔을때
-function handleMouseEnter() {
-  console.log('mouse is here');
-  h1.innerText = 'Mouse is here!';
-}
+/* 3.5
+  //onmouseenter
+  // hover처럼? 클릭이아니라 마우스가 요소의 범위내로 들어왔을때
+  function handleMouseEnter() {
+    console.log('mouse is here');
+    h1.innerText = 'Mouse is here!';
+  }
 
-h1.addEventListener('mouseenter', handleMouseEnter);
+  h1.addEventListener('mouseenter', handleMouseEnter);
 
-const handleMouseLeave = () => {
-  console.log('mouse is leaved');
-  h1.innerText = 'Mouse is leaved!';
-  h1.style.color = 'black';
-};
+  const handleMouseLeave = () => {
+    console.log('mouse is leaved');
+    h1.innerText = 'Mouse is leaved!';
+    h1.style.color = 'black';
+  };
 
-h1.addEventListener('mouseleave', handleMouseLeave);
+  h1.addEventListener('mouseleave', handleMouseLeave);
 
-function handleWindowResize() {
-  document.body.style.backgroundColor = 'tomato';
-}
+  function handleWindowResize() {
+    document.body.style.backgroundColor = 'tomato';
+  }
 
-// window내에서 복사가 사용됐을때 작동함
-function handleWindowCopy() {
-  alert('copier');
-}
+  // window내에서 복사가 사용됐을때 작동함
+  function handleWindowCopy() {
+    alert('copier');
+  }
 
-window.addEventListener('resize', handleWindowResize);
+  window.addEventListener('resize', handleWindowResize);
 
-// window내에서 복사가 사용됐을때 작동함
-window.addEventListener('copy', handleWindowCopy);
+  // window내에서 복사가 사용됐을때 작동함
+  window.addEventListener('copy', handleWindowCopy);
 
-function handleWindowOffline() {
-  alert('SOS no WIFI');
-}
+  function handleWindowOffline() {
+    alert('SOS no WIFI');
+  }
 
-function handleWindowOnline() {
-  alert('all good!');
-}
+  function handleWindowOnline() {
+    alert('all good!');
+  }
 
-window.addEventListener('offline', handleWindowOffline);
-window.addEventListener('online', handleWindowOnline);
+  window.addEventListener('offline', handleWindowOffline);
+  window.addEventListener('online', handleWindowOnline);
+*/
