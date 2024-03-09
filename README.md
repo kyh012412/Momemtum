2ac483ed6a86cb3746db9091e2d175197e129914
kyh012412 <kyh0124.12@gmail.com>
2024-03-09 16:45:46 +0900

    end3.5

--- README.md
+++ README.md
@@ -16,3 +16,23 @@ let element = documnet.querySelector('.condition');
 console.dir(element);
 // 출력된 결과로 가서 on~으로 시작하는것들을 확인
 ```
+
+## window
+
+[window문서](https://developer.mozilla.org/en-US/docs/Web/API/window)
+
+```js
+hello.addEventListener('click', handleHelloClick);
+```
+
+```js
+hello.onclick = handleHelloClick;
+```
+
+위 두개는 기능상 같은 동작을 할 수 있다.
+
+addEventListener로 추가한것은 추후에 희망시에
+removeEventListener로 제거 할 수 있다.
+
+그 외에 많은 것들을 부착할수 있다.
+window 기준 ex) copy online offline resize 등..
--- app.js
+++ app.js
@@ -1,30 +1,56 @@
-const hello = document.querySelector('div.hello:first-child h1');
+const h1 = document.querySelector('div.hello:first-child h1');
 // const title2 = document.querySelector('#hello:first-child');
 
-console.log(hello);
+console.log(h1);
 
-hello.innerText = 'Hello';
+h1.innerText = 'Hello';
 
 function handleHelloClick() {
-  hello.style.color = 'blue';
+  h1.style.color = 'blue';
   console.log('title was clicked');
 }
 
-hello.addEventListener('click', handleHelloClick);
+// hello.onclick = handleHelloClick;
+h1.addEventListener('click', handleHelloClick);
 
 //onmouseenter
 // hover처럼? 클릭이아니라 마우스가 요소의 범위내로 들어왔을때
 function handleMouseEnter() {
   console.log('mouse is here');
-  hello.innerText = 'Mouse is here!';
+  h1.innerText = 'Mouse is here!';
 }
 
-hello.addEventListener('mouseenter', handleMouseEnter);
+h1.addEventListener('mouseenter', handleMouseEnter);
 
 const handleMouseLeave = () => {
   console.log('mouse is leaved');
-  hello.innerText = 'Mouse is leaved!';
-  hello.style.color = 'black';
+  h1.innerText = 'Mouse is leaved!';
+  h1.style.color = 'black';
 };
 
-hello.addEventListener('mouseleave', handleMouseLeave);
+h1.addEventListener('mouseleave', handleMouseLeave);
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = 'tomato';
+}
+
+// window내에서 복사가 사용됐을때 작동함
+function handleWindowCopy() {
+  alert('copier');
+}
+
+window.addEventListener('resize', handleWindowResize);
+
+// window내에서 복사가 사용됐을때 작동함
+window.addEventListener('copy', handleWindowCopy);
+
+function handleWindowOffline() {
+  alert('SOS no WIFI');
+}
+
+function handleWindowOnline() {
+  alert('all good!');
+}
+
+window.addEventListener('offline', handleWindowOffline);
+window.addEventListener('online', handleWindowOnline);
