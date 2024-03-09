a31b23e255630291fc3e907facb5cb57d50c6ef5
kyh012412 <kyh0124.12@gmail.com>
2024-03-09 09:18:30 +0900

    end3.3

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const title = document.querySelector('.hello h1');
+const hello = document.querySelector('div.hello:first-child h1');
 // const title2 = document.querySelector('#hello:first-child');
 
-title.innerText = 'Hello';
+console.log(hello);
+
+hello.innerText = 'Hello';
+
+function handleHelloClick() {
+  hello.style.color = 'blue';
+  console.log('title was clicked');
+}
+
+hello.addEventListener('click', handleHelloClick);
