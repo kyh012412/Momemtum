b15e7edfd29cb2519a32c24db3932ee1858eaf44
kyh012412 <kyh0124.12@gmail.com>
2024-03-09 09:39:24 +0900

    end3.4

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Momentum clone 코딩
+
+## 필요한 eventListener을 찾는 법
+
+###
+
+[h1 html element mdn](https://developer.mozilla.org/en-US/docs/Web/API/HTMLHeadingElement)를 검색
+
+또는
+
+###
+
+```js
+let element = documnet.querySelector('.condition');
+
+console.dir(element);
+// 출력된 결과로 가서 on~으로 시작하는것들을 확인
+```
--- app.js
+++ app.js
@@ -11,3 +11,20 @@ function handleHelloClick() {
 }
 
 hello.addEventListener('click', handleHelloClick);
+
+//onmouseenter
+// hover처럼? 클릭이아니라 마우스가 요소의 범위내로 들어왔을때
+function handleMouseEnter() {
+  console.log('mouse is here');
+  hello.innerText = 'Mouse is here!';
+}
+
+hello.addEventListener('mouseenter', handleMouseEnter);
+
+const handleMouseLeave = () => {
+  console.log('mouse is leaved');
+  hello.innerText = 'Mouse is leaved!';
+  hello.style.color = 'black';
+};
+
+hello.addEventListener('mouseleave', handleMouseLeave);
