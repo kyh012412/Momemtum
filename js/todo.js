8d4d70fb2c675d56a3125dd8c996fad61d5b7a26
kyh012412 <kyh0124.12@gmail.com>
2024-03-10 20:14:28 +0900

    7.1 todolist(storage X)

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.querySelector('#todo-form');
 const todoInput = toDoForm.querySelector('input');
 const toDoList = document.querySelector('#todo-list');
 
+function paintToDo(newTodo) {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  span.innerText = newTodo;
+  li.appendChild(span);
+  toDoList.append(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = '';
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
