645557c2d7c401bb3a15cf424142bfd2150a1c9e
kyh012412 <kyh0124.12@gmail.com>
2024-03-10 21:05:21 +0900

    7.2 end deleting to dos

--- js/todo.js
+++ js/todo.js
@@ -1,18 +1,33 @@
 const toDoForm = document.querySelector('#todo-form');
 const todoInput = toDoForm.querySelector('input');
 const toDoList = document.querySelector('#todo-list');
+let todoJson = [];
 
 function paintToDo(newTodo) {
   const li = document.createElement('li');
+
   const span = document.createElement('span');
   span.innerText = newTodo;
+
+  const button = document.createElement('button');
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
+
   li.appendChild(span);
+  li.appendChild(button);
   toDoList.append(li);
+}
+
+function deleteToDo(e) {
+  const li = e.currentTarget.parentElement;
+  console.dir(li);
+  li.remove();
 }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
+  todoJson = [...todoJson, newTodo];
   todoInput.value = '';
   paintToDo(newTodo);
 }
