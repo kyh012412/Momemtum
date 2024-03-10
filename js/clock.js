ca170ad10fce83ae8ab71f9b5f20e22f208a0630
kyh012412 <kyh0124.12@gmail.com>
2024-03-10 16:10:31 +0900

    end5.1

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,19 @@
 const clock = document.querySelector('h2#clock');
 
-function sayHello() {
-  console.log('hello');
+function getClock() {
+  const date = new Date();
+  const Hours = setLong(date.getHours());
+  const Minutes = setLong(date.getMinutes());
+  const Seconds = setLong(date.getSeconds());
+  clock.innerHTML = `${Hours}:${Minutes}:${Seconds}`;
 }
 
-setInterval(sayHello, 1000);
+getClock();
+
+setInterval(getClock, 1000);
+
+function setLong(num) {
+  return num < 10 ? '0' + num : num;
+}
+
+// setTimeout(getClock, 5000);
