eb6b6c447778ebfbe195610686c09194ba7c06c2
kyh012412 <kyh0124.12@gmail.com>
2024-03-10 11:58:30 +0900

    end4.3

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector('input');
 
+const link = document.querySelector('a');
+
 function onLoginBtnClick(event) {
   event.preventDefault();
   const username = loginInput.value;
@@ -8,12 +10,13 @@ function onLoginBtnClick(event) {
   console.log(username);
 }
 
-// form의 submit이 발생하는 조건은 엔터를 누르거나 button이 눌릴때 발생함
 loginForm.addEventListener('submit', onLoginBtnClick);
 
-// 여기에서 콜백함수에는 ()를 붙여주지 않는다. 그것은 이곳에서 죽시 실행한다는 뜻이기 때문이다.
-// loginForm.addEventListener('submit', onLoginBtnClick());
+function handleLinkClick(event) {
+  console.dir(event);
+  // alert('clicked'); //alert를 사용하면 모든 js를 막음
+  event.preventDefault();
+}
 
-// 정상적으로 작동은 하지만 browser가 form 이 submit 될때 refresh하도록 되어 있음
-
-//모든 EventListener function의 첫번째 argument는 항상 방금일어난 정보를 지닌다.
+link.addEventListener('click', handleLinkClick);
+// 뒤에 함수에 javascript가 필요한 정보를 자동으로 넣어준다.(ex event)
